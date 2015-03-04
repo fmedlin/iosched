43289ea5c3ee0148396ad36866e4ed2debc4c67d
Fred Medlin <fred@medl.in>
2015-03-04 18:55:21 -0500

    Setup a robolectric test environment and add a failing robolectric test

---
+++ android/src/test/java/com/google/samples/apps/iosched/ui/widget/MyScheduleActivityTest.java
@@ -0,0 +1,13 @@
+package com.google.samples.apps.iosched.ui.widget;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.fail;
+
+public class MyScheduleActivityTest {
+
+    @Test
+    public void itShouldFail() {
+        fail("Implement me");
+    }
+}
